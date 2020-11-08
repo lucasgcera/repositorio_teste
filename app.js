1f476fefe2f8fed18103785de2e20ad13607cdd9
lucas.cera <lucas_gcera@hotmail.com>
2020-11-08 15:05:31 -0300

    Encapsula variaveis de ambiente com dotenv

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { accountRouter } from './routes/accountRouter.js';
 
+require('dotenv').config();
+
 //conectar ao MongoDB pelo Mongoose
 (async () => {
     try {
         await mongoose.connect(
-            'mongodb+srv://userlucas:igtiuser@cluster0.7uu6j.mongodb.net/bank?retryWrites=true&w=majority',
+            `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@cluster0.7uu6j.mongodb.net/bank?retryWrites=true&w=majority`,
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
@@ -24,4 +26,4 @@ const app = express();
 app.use(express.json());
 app.use(accountRouter); //adicionando arquivos com as rotas para a API
 
-app.listen(3000, () => console.log('API Iniciada')); //iniciando API na porta 3000
+app.listen(process.env.PORT, () => console.log('API Iniciada')); //iniciando API na porta 3000
