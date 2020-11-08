0db10c9fc8165e78dfdb622333826340405bb4da
lucas.cera <lucas_gcera@hotmail.com>
2020-11-08 18:23:36 -0300

    "Ajustes da importacao do dotenv"

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { accountRouter } from './routes/accountRouter.js';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
+dotenv.config();
 
 //conectar ao MongoDB pelo Mongoose
 (async () => {
@@ -23,7 +24,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); //defini para a API usar arquivos JSON
 app.use(accountRouter); //adicionando arquivos com as rotas para a API
 
 app.listen(process.env.PORT, () => console.log('API Iniciada')); //iniciando API na porta 3000
